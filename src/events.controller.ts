be8edeb5655bf90eef662903969c42287d12d62e
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-09-28 19:56:29 +0530

    ✅ setup basic controller setup

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,19 +1,56 @@
-import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAllResource() {}
+  findAllResource() {
+    return [
+      {
+        id: 1,
+        name: 'First Event',
+      },
+      {
+        id: 2,
+        name: 'Second Event',
+      },
+      {
+        id: 3,
+        name: 'Third Event',
+      },
+    ];
+  }
 
-  @Get()
-  findOneResource() {}
+  @Get(':id')
+  findOneResource(@Param('id') id: [string, number]) {
+    return {
+      id: id,
+      name: 'Second Event',
+    };
+  }
 
   @Post()
-  createResource() {}
+  createResource(@Body() input) {
+    return input;
+  }
 
-  @Patch()
-  updateResource() {}
+  @Patch(':id')
+  updateResource(@Param('id') id: [string, number], @Body() input) {
+    return {
+      id,
+      input: input,
+    };
+  }
 
-  @Delete()
-  deleteResource() {}
+  @Delete(':id')
+  deleteResource(@Param('id') id: [string, number]) {
+    return id;
+  }
 }
